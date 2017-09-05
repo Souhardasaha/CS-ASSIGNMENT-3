ebfe11f3e4827eb0a7651f19d29d04bf84d29d75
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-05 16:58:16 +0530

    Create Lab3_question6.cpp

---
+++ Lab3_question6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{    int a;
+     cout<<" enter the number of days of the year ";
+     cin>>a;
+     if(a%365==0)
+       cout<<" it is not a leap year ";
+     else
+        cout<<" it is a leap year ";
+}
