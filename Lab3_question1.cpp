35f35ed81d703fbdf42f26ca7afff83b9884416e
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-05 16:51:47 +0530

    Create Lab3_question1.cpp

---
+++ Lab3_question1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{     int a,b,max;
+      cout<<" enter 1st number ";
+      cin>>a;
+      cout<<" enter 2nd number ";
+      cin>>b;
+      if(a>=b)
+        max=a;
+      else
+          max=b;
+      cout<<" maximum: "<<max;
+}
