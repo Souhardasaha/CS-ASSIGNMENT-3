f02b7e2c7433007b18e1094b718985af266f37fa
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-05 16:56:32 +0530

    Create Lab3_question4.cpp

---
+++ Lab3_question4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{    int a;
+     cout<<" enter a number ";
+     cin>>a;
+     if(a%5==0 && a%11==0)
+       cout<<" the number is divisible by 5 and 11 ";
+     else 
+         cout<<" the number is not divisible by 5 and 11 ";
+}
