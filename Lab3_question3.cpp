50f9e7797ca836a636cd8ec5b11776a69237a51a
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-05 16:55:48 +0530

    Create Lab3_question3.cpp

---
+++ Lab3_question3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{    int a;
+     cout<<" enter a number ";
+     cin>>a;
+     if(a<0)
+       cout<<" the number is negative ";
+     else
+     if(a>0)
+       cout<<" the number is positive ";
+     else 
+         cout<< " the number is zero ";
+}
