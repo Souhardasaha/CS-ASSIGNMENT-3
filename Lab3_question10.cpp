008e1cb38facbf5b31aeabbd7d11ac7563119ca4
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-05 17:22:09 +0530

    Create Lab3_question10.cpp

---
+++ Lab3_question10.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{    char a;
+     cout<<" enter a character ";
+     cin>>a;
+     if(a>=65 && a<=90)
+       cout<<" the character is uppercase alphabet ";
+     else 
+        cout<<" the character is a lowercase alphabet ";
+}
