0ffc0c5b185ecf68c0869226f955436fe1000501
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-07 18:54:25 +0530

    Create Lab3_question11.cpp

---
+++ Lab3_question11.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+main()
+{    int a;
+     cout<<" enter week day number ";
+     cin>>a;
+     if(a==1)
+       cout<<" the week day is monday ";
+     else;
+     if(a==2)
+       cout<<" the week day is tuesday ";
+     else;
+     if(a==3)
+       cout<<" the week day is wednesday ";
+     else
+     if(a==4)
+       cout<<" the week day is thruday ";
+     else
+     if(a==5)
+       cout<<" the week day is friday ";
+     else
+     if(a==6)
+       cout<<" the week day is saturday ";
+     else
+     if(a==7)
+       cout<<" the week day is sunday ";
+     else
+       cout<<" invalid week day number ";
+}
