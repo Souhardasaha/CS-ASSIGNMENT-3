4a160ddb51c8c867c2994a05ec28826f9aed5cde
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-09 17:51:54 +0530

    Create Lab3_question21.cpp

---
+++ Lab3_question21.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main() {
+	float a,b;
+	cout<<" enter electric units used: ";
+	cin>>a;
+	if(a<=50)
+	b=a*0.5;
+	else 
+	if(a>50 & a<=150)
+	b=(50*0.5)+(0.75*(a-50));
+	else
+	if(a>150 & a<=250)
+	b=100+(1.20*(a-150)); 
+	else  
+	b=220+(1.5*(a-250));
+	b+=0.2*b;
+	cout<<" total electricity bill: "<<b;
+}
