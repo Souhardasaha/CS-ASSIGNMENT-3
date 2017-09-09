39062dc7e971d910857babd156e27cfdfc4a44ad
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-09 16:18:28 +0530

    Update Lab3_question22.cpp

--- Lab3_question22.cpp
+++ Lab3_question22.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 main() {
-	int a,i=1;
-	cout<<" program to print all natural numbers from 1 to n. - using while loop ";
+	int a,b=1;
+	cout<<"  ";
 	cin>>a;
-	while (i<=a){
-		cout<<i;
-		i++;
+	while (b<=a){
+		cout<<b<<"  ";
+		b++;
 	}
 }
